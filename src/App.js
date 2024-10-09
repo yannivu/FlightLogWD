3b70b56de3be9f66f982b4ffc20ffde89b2d97d0
Andrew Vittiglio <andrewv433@gmail.com>
2024-10-09 14:16:49 -0400

    AV: first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import * as ENV from './environments.js';
+import Parse from 'parse';
+
+Parse.initialize(ENV.APPLICATION_ID, ENV.JAVASCRIPT_KEY);
+Parse.serverURL(ENV.SERVER_URL);
 
 function App() {
   return (
